5c25ec9d20a8692eef1c36169a52eeacd9f90e37
aryarrama <aryar5945@gmail.com>
2025-09-20 22:30:27 +0700

    Update latihan 1 no5.cpp

--- latihan 1 no5.cpp
+++ latihan 1 no5.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main() {
     int A, B;
     cout<<"Program menukar nilai"<<endl<<endl;
-    cout << "Masukkan nilai A: ";
-    cin >> A;
+    cout<<"Masukkan nilai A: ";
+    cin>>A;
     
-    cout << "Masukkan nilai B: ";
-    cin >> B;
+    cout<<"Masukkan nilai B: ";
+    cin>>B;
     
     int temp = A;
     A = B;
     B = temp;
     
-    cout << "Hasil:" << endl;
-    cout << "A = " << A << endl;
-    cout << "B = " << B << endl;
+    cout<<"Hasil:"<<endl;
+    cout<<"A = "<< A <<endl;
+    cout<<"B = "<< B << endl;
     
     return 0;
 }
